93b17bf1e81f83d370e3ad499174c78ec33063d6
if1live <libsora25@gmail.com>
2015-07-03 23:23:33 -0400

    pyenv로 python 3.4.3 설치 성공

--- bootstrap.sh
+++ bootstrap.sh
@@ -61,6 +61,12 @@ function install_nvm {
 }
 
 function install_pyenv {
+	# http://stackoverflow.com/questions/12806122/missing-python-bz2-module
+	# 추가 패키지를 설치하지 않으면 python 3.4.3 설치중 에러가 뜬다
+	# WARNING: The Python bz2 extension was not compiled. Missing the bzip2 lib?
+	sudo apt-get install -qq libbz2-dev
+	#sudo yum install libbz2-devel // or bzip2-devel
+
 	[ -z "$PYENV_ROOT" ] && export PYENV_ROOT="$HOME/.pyenv"
 
 	if [ ! -d $PYENV_ROOT ]; then
