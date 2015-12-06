44381f96a35a74844d1164412974032548a3eeff
if1live <libsora25@gmail.com>
2015-12-06 21:32:13 +0900

    company-go

--- emacs/emacs.d/config/coding.el
+++ emacs/emacs.d/config/coding.el
@@ -3,3 +3,6 @@
 ;; enable in all programming-related modes
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 ;; // rainbow-delemiters
+
+;; company-mode
+(add-hook 'after-init-hook 'global-company-mode)
--- emacs/emacs.d/config/modes/programming/go-mode.el
+++ emacs/emacs.d/config/modes/programming/go-mode.el
@@ -1,20 +1,31 @@
 ;; http://andrewjamesjohnson.com/configuring-emacs-for-go-development/
 
-(require 'go-autocomplete)
-(require 'auto-complete-config)
-(ac-config-default)
-
 (defun my-go-before-save-hook ()
   (gofmt-before-save)
   (delete-trailing-whitespace))
 
 (defun go-mode-setup ()
+  ;; https://github.com/nsf/gocode/tree/master/emacs-company
+  ;; auto complete
+  (setq company-tooltip-limit 20)                      ; bigger popup window
+  (setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+  (setq company-echo-delay 0)                          ; remove annoying blinking
+  (setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+
+  (set (make-local-variable 'company-backends) '(company-go))
+  (company-mode)
+
+  ;; goimport
+  (setq gofmt-command "goimports")
+
+  ;; keyboard
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (define-key (current-local-map) "\C-c\C-c" 'compile)
+
+  ;; hook
   (add-hook 'before-save-hook 'my-go-before-save-hook)
   (setq compile-command "go build -v && go test -v && go vet")
-  (define-key (current-local-map) "\C-c\C-c" 'compile)
-  (go-eldoc-setup)
-  (setq gofmt-command "goimports")
-  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
-  (local-set-key (kbd "M-.") 'godef-jump))
+
+  (go-eldoc-setup))
 
 (add-hook 'go-mode-hook 'go-mode-setup)
