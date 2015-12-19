5af1658cb681f5f331ffdca397bf687ee57c1aba
if1live <libsora25@gmail.com>
2015-12-19 13:37:08 +0900

    c# coding style

--- emacs/emacs.d/config/modes/programming/csharp-mode.el
+++ emacs/emacs.d/config/modes/programming/csharp-mode.el
@@ -1,11 +1,9 @@
+(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
 (add-to-list 'auto-mode-alist '("\\.cs" . csharp-mode))
 
-(defun my-csharp-before-save-hook ()
-  (delete-trailing-whitespace))
-
-(defun cshart-mode-setup ()
+(defun csharp-mode-setup ()
   ;; indent
-  (c-set-style "java")
+  (c-set-style "c#")
   (setq tab-width 4)
   (setq c-basic-offset 4)
   (setq indent-tabs-mode nil)
@@ -14,6 +12,6 @@
   (electric-pair-mode 1)
 
   ;; hook
-  (add-hook 'before-save-hook 'my-csharp-before-save-hook))
+  (add-hook 'before-save-hook 'delete-trailing-whitespace))
 
 (add-hook 'csharp-mode-hook 'csharp-mode-setup)
