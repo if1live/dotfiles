381d416f381f7e087d923a5e5f62199e9e00d53c
if1live <libsora25@gmail.com>
2016-02-26 17:03:46 +0900

    마크다운 확장자 다를떄도 강제로 적용할라고

--- emacs/emacs.d/config/modes/markdown-mode.el
+++ emacs/emacs.d/config/modes/markdown-mode.el
@@ -4,6 +4,11 @@
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+;; 정적 사이트 생성기용 markdown 생성기 maya 때문에 추가한것
+;; 템플릿의 확장자를 .md로 유지하면 pelican이 의도치않게 읽어버린다
+(add-to-list 'auto-mode-alist '("\\.md.tpl$" . markdown-mode))
+
 (add-hook 'markdown-mode-hook
   (lambda ()
     (setq markdown-coding-system 'utf-8)
