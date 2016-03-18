afa9cf0b3b6028b11a7b14e7b17630cd22e962a0
if1live <libsora25@gmail.com>
2016-03-18 23:48:22 +0900

    마크다운 대응 확장자 추가

--- emacs/emacs.d/config/modes/markdown-mode.el
+++ emacs/emacs.d/config/modes/markdown-mode.el
@@ -1,13 +1,12 @@
 ;; http://jblevins.org/projects/markdown-mode/
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
+
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mkdn$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
-
-;; 정적 사이트 생성기용 markdown 생성기 maya 때문에 추가한것
-;; 템플릿의 확장자를 .md로 유지하면 pelican이 의도치않게 읽어버린다
-(add-to-list 'auto-mode-alist '("\\.md.tpl$" . markdown-mode))
 
 (add-hook 'markdown-mode-hook
   (lambda ()
