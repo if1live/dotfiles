e8217fd83902767be72a15e57238d9918e088d2d
if1live <libsora25@gmail.com>
2017-04-23 20:38:42 +0900

    tmux에서 ctrl-b를 그대로 쓰게함 이맥스에서 ctrl-b를 쓰지 않도록 함

--- emacs/emacs.d/config/keymap.el
+++ emacs/emacs.d/config/keymap.el
@@ -4,3 +4,8 @@
 ;; magit
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
+;; tmux에서 ctrl-b를 그대로 쓰고싶다
+;; 그래서 기본 단축키 제거
+(dolist (key '("\C-b"))
+  (global-unset-key key))
