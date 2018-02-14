67778ed8308e9e29177ae45bd8b5b19d268ad40e
if1live <libsora25@gmail.com>
2018-02-14 23:13:48 +0900

    go package install script

---
+++ install/core-go.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+go get -u github.com/golang/dep/cmd/dep
+
--- install/dev-go.sh
+++ install/dev-go.sh
@@ -1,6 +1,52 @@
 #!/bin/bash
 
-go get -u github.com/nsf/gocode
-go get -u golang.org/x/tools/cmd/goimports
-go get -u github.com/Perlmint/goautoenv
-go get -u github.com/tools/godep
+# vscode-go
+
+# An autocompletion daemon for the Go programming language
+go get -u -v github.com/nsf/gocode
+
+# Print where symbols are defined in Go source code
+go get -u -v github.com/rogpeppe/godef
+
+# Utility to extract JSON representation of declarations from a Go source file 
+go get -u -v github.com/ramya-rao-a/go-outline
+
+# A gofmt/goimports-like tool for Go programmers that fills in Go return statements with zero values to match the func return types
+go get -u -v github.com/sqs/goreturns
+
+# The gorename command performs precise type-safe renaming of identifiers in Go source code.
+go get -u -v golang.org/x/tools/cmd/gorename
+
+# Command gopkgs list your installed Go packages for import.
+go get -u -v github.com/tpng/gopkgs
+
+# A utility for extracting a JSON representation of the package symbols from a go source tree.
+go get -u -v github.com/acroca/go-symbols
+
+# Generate Go tests from your source code.
+go get -u -v github.com/cweill/gotests/...
+
+# Gets documentation for items in Go source code.
+go get -u -v github.com/zmb3/gogetdoc
+
+# This is a linter for Go source code.
+go get -u -v github.com/golang/lint/golint
+
+# Godoc extracts and generates documentation for Go programs.
+go get -u -v golang.org/x/tools/cmd/godoc
+
+# guru: a tool for answering questions about Go source code.
+go get -u -v golang.org/x/tools/cmd/guru
+
+# Concurrently run Go lint tools and normalise their output
+go get -u -v github.com/alecthomas/gometalinter
+
+# Command goimports updates your Go import lines, adding missing ones and removing unreferenced ones.
+go get -u -v golang.org/x/tools/cmd/goimports
+
+# Go tool to modify struct field tags
+go get -u -v github.com/fatih/gomodifytags
+
+
+
+#go get -u github.com/Perlmint/goautoenv
