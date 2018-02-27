ac0a17ea6276188809d10a1b7c8921b8ee368153
if1live <libsora25@gmail.com>
2018-02-27 15:41:31 +0900

    ubuntu 16.04 + python3.6

---
+++ install/dist-python3.6.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# https://www.rosehosting.com/blog/how-to-install-python-3-6-on-ubuntu-16-04/
+sudo apt-get install software-properties-common python-software-properties
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install python3.6
+
+
