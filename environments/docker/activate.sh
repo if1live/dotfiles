40f2cf2d698a5382b180809c107f6a04d04ab5de
if1live <libsora25@gmail.com>
2018-04-04 14:24:27 +0900

    DOCKER_HOST는 WSL에서만 돌아가도록

--- environments/docker/activate.sh
+++ environments/docker/activate.sh
@@ -1,3 +1,6 @@
 # for WSL
 # https://medium.com/@sebagomez/installing-the-docker-client-on-ubuntus-windows-subsystem-for-linux-612b392a44c4
-export DOCKER_HOST=localhost:2375
+
+if [[ $OS_WSL == "1" ]]; then
+  export DOCKER_HOST=localhost:2375
+fi
