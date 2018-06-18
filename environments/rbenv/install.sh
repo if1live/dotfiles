24493035476aa57d97786cb5bc18c1802d0be020
if1live <libsora25@gmail.com>
2018-06-18 06:22:38 +0000

    local은 스크립트 함수 밖에서는 못쓴다

--- environments/rbenv/install.sh
+++ environments/rbenv/install.sh
@@ -9,7 +9,7 @@ else
 	echo "rbenv already exist"
 fi
 
-local RUBY_BUILD_PATH="$RBENV_ROOT/plugins/ruby-build"
+RUBY_BUILD_PATH="$RBENV_ROOT/plugins/ruby-build"
 if [ ! -d $RUBY_BUILD_PATH ]; then
 	git clone git://github.com/sstephenson/ruby-build.git $RUBY_BUILD_PATH
 else
@@ -18,7 +18,7 @@ fi
 
 # https://github.com/rkh/rbenv-update
 mkdir -p "$RBENV_ROOT/plugins"
-local RBENV_UPDATE_PATH="$RBENV_ROOT/plugins/rbenv-update"
+RBENV_UPDATE_PATH="$RBENV_ROOT/plugins/rbenv-update"
 if [ ! -d $RUBY_BUILD_PATH ]; then
 	git clone https://github.com/rkh/rbenv-update.git $RBENV_UPDATE_PATH
 else
