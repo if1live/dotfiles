d02afb33f314a402f0bc7389f9d9cece28aee79e
byunghoo.yu <byunghoo.yu@5minlab.com>
2026-01-12 10:07:57 +0900

    wezterm 터미널 소리 끄기
    wsl bash에서 backspace 눌렀는데 지울게 없을때 뜨는 소리 끄기

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -12,5 +12,8 @@ config.color_scheme = 'Material Darker (base16)'
 -- bash configuration
 config.default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe", "--login", "-i" }
 
+-- 비프음(소리) 끄기
+config.audible_bell = "Disabled"
+
 -- and finally, return the configuration to wezterm
 return config
